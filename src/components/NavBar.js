ce6ce0e7c64fb20f84f45b9908be6f539e6c64aa
stephen mburu <stephenmburu8509@gmail.com>
2025-05-29 14:15:30 +0300

    adds lista

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function NavBar() {
-  const links = ["home", "about", "projects"];
+const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+function NavBar() {
+  return (
+    <nav>
+      {links.map((link) => (
+        <a key={link} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
